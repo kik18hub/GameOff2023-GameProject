3458150d8d4cf02dd4de0b805f79683fa999f77a
XorsDomain <georgemarshall23@gmail.com>
2023-11-07 19:54:10 -0500

    Changed input

--- player/player.gd
+++ player/player.gd
@@ -2,29 +2,38 @@ extends CharacterBody2D
 
 const SPEED = 70
 
-var input_direction = Vector2.ZERO
-var sprite_direction = "Down"
+var input_direction: get = _get_input_direction 
+var sprite_direction = "Down": get = _get_sprite_direction
 
 @onready var sprite = $AnimatedSprite2D
 
 func _physics_process(_delta):
-	input_direction = _get_input_direction()
 	velocity = input_direction * SPEED
 	move_and_slide()
 
-	set_animation(sprite_direction)
+	set_animation("walkDown")
 	if velocity == Vector2.ZERO:
 		sprite.stop()
 
 func set_animation(animation):
-	var direction = sprite_direction
-	if sprite_direction in ["Left", "Right"]:
-		sprite.play("walkSide")
-	sprite.play("walkDown")
+	var direction = "Side" if sprite_direction in ["Left", "Right"] else sprite_direction
+	sprite.play("walkSide" + "Right")
 	sprite.flip_h = (sprite_direction == "Left")
 
 func _get_input_direction():
-	return Vector2(-int(Input.is_action_pressed("ui_left")) + int(Input.is_action_pressed("ui_right")), -int(Input.is_action_pressed("ui_up")) + int(Input.is_action_pressed("ui_down")))
+	var x = -int(Input.is_key_label_pressed(KEY_A)) + int(Input.is_key_label_pressed(KEY_D))
+	var y = -int(Input.is_key_label_pressed(KEY_W)) + int(Input.is_key_label_pressed(KEY_S))
+	input_direction = Vector2(x,y).normalized()
+	return input_direction
 
 func _get_sprite_direction():
-	return input_direction.normalized()
+	match input_direction:
+		Vector2.LEFT:
+			sprite_direction = "Left"
+		Vector2.RIGHT:
+			sprite_direction = "Right"
+		Vector2.UP:
+			sprite_direction = "Up"
+		Vector2.DOWN:
+			sprite_direction = "Down"
+	return sprite_direction

80c18e64a450cb001f59f429e56579eaacd8efe6
Yilun-Sun <yilunsun11@yeah.net>
2023-11-20 00:03:44 +0800

    feat: change world generation

--- scripts/room_generation.gd
+++ scripts/room_generation.gd
@@ -9,21 +9,117 @@ enum RoomTypes {
 	EMPTY
 }
 
-var floor_count := 5
-var room_count_per_floor := 3
+enum DoorTypes {
+	TOP,
+	RIGHT,
+	BOTTOM,
+	LEFT
+}
+
+enum Directions {
+	TOP,
+	RIGHT,
+	BOTTOM,
+	LEFT
+}
+
+const DIRECTIONS = [Vector2(0, -1), Vector2(1, -0), Vector2(0, 1), Vector2(-1, 0)]
+
+var floor_count := 7
+var room_count_per_floor := 5
 var battle_room_count_per_floor := 1
 var chest_room_count_per_floor := 1
 var block_room_count_per_floor := 1
 
 func generate_rooms():
-	var rooms = [].resize(floor_count)
-	for 
+	var all_rooms: Array = []
+	all_rooms.resize(floor_count + 2)
+	for floor in range(len(all_rooms)):
+		all_rooms[floor] = []
+		all_rooms[floor].resize(room_count_per_floor)
+	
+	all_rooms[0][floor(room_count_per_floor / 2)] = {
+		type = RoomTypes.END,
+		doors = [DoorTypes.BOTTOM]
+	}
+	all_rooms[floor_count + 1][floor(room_count_per_floor / 2)] = {
+		type = RoomTypes.START,
+		doors = [DoorTypes.TOP]
+	}
 	
 	if room_count_per_floor < battle_room_count_per_floor + chest_room_count_per_floor + block_room_count_per_floor:
 		return []
-	var rooms = [Vector2(0, 0)]
-	for i in range(floor_count):
-		for j in range(room_count_per_floor):
-			rooms.append(Vector2(j - floor(room_count_per_floor / 2), - (i + 1)))
-	rooms.append(Vector2(0, - (floor_count + 1)))
-	return rooms
+	
+	# generate rooms
+	for f in range(1, floor_count + 1):
+		for r in range(room_count_per_floor):
+			all_rooms[f][r] = {
+				type = RoomTypes.EMPTY
+			}
+	
+	# change room type
+	for f in range(1, floor_count + 1):
+		var battle_room_count = battle_room_count_per_floor
+		var chest_room_count = chest_room_count_per_floor
+		var block_room_count = block_room_count_per_floor
+		var room_index_in_floor = []
+		
+		# randomly pick room in a floor
+		for r in range(room_count_per_floor):
+			room_index_in_floor.append(r)
+		room_index_in_floor.shuffle()
+		
+		for r in room_index_in_floor:
+			var available_types = [RoomTypes.EMPTY]
+			if battle_room_count > 0:
+				available_types.append(RoomTypes.BATTLE)
+			if chest_room_count > 0:
+				available_types.append(RoomTypes.CHEST)
+			if block_room_count > 0:
+				available_types.append(RoomTypes.BLOCK)
+				
+			if (all_rooms[f - 1][r] != null and all_rooms[f - 1][r].type == RoomTypes.END) or (all_rooms[f + 1][r] != null and all_rooms[f + 1][r].type == RoomTypes.START):
+				available_types = available_types.filter(func(type): return type != RoomTypes.BLOCK)
+				
+			var pick_type = available_types[randi_range(0, len(available_types) - 1)]
+			match pick_type:
+				RoomTypes.BATTLE:
+					battle_room_count -= 1
+				RoomTypes.CHEST:
+					chest_room_count -= 1
+				RoomTypes.BLOCK:
+					block_room_count -= 1
+			
+			all_rooms[f][r] = {
+				type = pick_type
+			}
+			
+	# add doors		
+	for f in range(1, floor_count + 1):
+		for r in range(room_count_per_floor):
+			var doors = []
+			for direction in DIRECTIONS:
+				var check_floor_index = f + direction.y
+				var check_room_index = r + direction.x
+				if check_floor_index == 0 or \
+					check_floor_index == floor_count + 2 or \
+					check_room_index < 0 or \
+					check_room_index >= room_count_per_floor or \
+					all_rooms[check_floor_index][check_room_index] == null or \
+					all_rooms[check_floor_index][check_room_index].type == RoomTypes.BLOCK:
+					continue
+				
+				match direction:
+					Vector2(0, -1):
+						doors.append(DoorTypes.TOP)
+					Vector2(1, 0):
+						doors.append(DoorTypes.RIGHT)
+					Vector2(0, 1):
+						doors.append(DoorTypes.BOTTOM)
+					Vector2(-1, 0):
+						doors.append(DoorTypes.LEFT)
+			all_rooms[f][r].doors = doors
+	
+	print(all_rooms)
+	return all_rooms
+
--- world.gd
+++ world.gd
@@ -5,50 +5,69 @@ const ROOM_SIZE = Vector2(320, 160)
 
 @export var floor_count := 5
 
+@onready var panel: Panel = $CanvasLayer/Panel
 @onready var mini_map: Node2D = $CanvasLayer/Panel/RoomRectContainer
 @onready var mini_map_marker_2d: Marker2D = $CanvasLayer/Panel/Marker2D
 @onready var room_container: Node2D = $RoomContainer
 @onready var player_indicator_rect: ColorRect = $CanvasLayer/Panel/PlayerIndicator
+@onready var player: CharacterBody2D = $Player
 
 
 var room_count_per_floor := 3
-var room_coors: Array
-
-
+var all_rooms: Array
 
 
 func _ready() -> void:
 	Events.root_entered.connect(_room_entered)
-	room_coors = RoomGeneration.generate_rooms()
+	all_rooms = RoomGeneration.generate_rooms()
 	_add_room()
+	player.position = Vector2(
+				ROOM_SIZE.x * floor(RoomGeneration.room_count_per_floor / 2 + 1) - ROOM_SIZE.x / 2,
+				ROOM_SIZE.y * (RoomGeneration.floor_count + 2) - ROOM_SIZE.y / 2)
 	_draw_mini_map()
 	
 
 func _add_room():
-	for room_coor in room_coors:
-		var room = ROOM.instantiate()
-		if room_coors.has(room_coor + Vector2(0, -1)):
-			room.has_up_door = true
-		if room_coors.has(room_coor + Vector2(1, 0)):
-			room.has_right_door = true
-		if room_coors.has(room_coor + Vector2(0, 1)):
-			room.has_down_door = true
-		if room_coors.has(room_coor + Vector2(-1, 0)):
-			room.has_left_door = true
-		room_container.add_child(room)
-		room.position = Vector2(
-			ROOM_SIZE.x * room_coor.x,
-			ROOM_SIZE.y * room_coor.y)
+	for f in range(RoomGeneration.floor_count + 2):
+		for r in range(RoomGeneration.room_count_per_floor):
+			var room = all_rooms[f][r]
+			if room == null:
+				continue
+				
+			var room_instance = ROOM.instantiate()
+			if room.doors.has(RoomGeneration.DoorTypes.TOP):
+				room_instance.has_up_door = true
+			if room.doors.has(RoomGeneration.DoorTypes.RIGHT):
+				room_instance.has_right_door = true
+			if room.doors.has(RoomGeneration.DoorTypes.BOTTOM):
+				room_instance.has_down_door = true
+			if room.doors.has(RoomGeneration.DoorTypes.LEFT):
+				room_instance.has_left_door = true
+			room_container.add_child(room_instance)
+			room_instance.position = Vector2(
+				ROOM_SIZE.x * r,
+				ROOM_SIZE.y * f)
 	
 	
 func _draw_mini_map():
-	for room_coor in room_coors:
-		var color_rect = ColorRect.new()
-		color_rect.size = Vector2(5, 5)
-		color_rect.color = Color.DARK_GRAY
-		mini_map.add_child(color_rect)
-		color_rect.position = room_coor * 6 + mini_map_marker_2d.position
-
+	mini_map_marker_2d.position.x = panel.size.x / 2 - RoomGeneration.room_count_per_floor / 2 * 6
+	for f in range(RoomGeneration.floor_count + 2):
+		for r in range(RoomGeneration.room_count_per_floor):
+			var room = all_rooms[f][r]
+			if room == null:
+				continue
+			var color_rect = ColorRect.new()
+			color_rect.size = Vector2(5, 5)
+			color_rect.color = Color.DARK_GRAY
+			match room.type:
+				RoomGeneration.RoomTypes.BATTLE:
+					color_rect.color = Color.FIREBRICK
+				RoomGeneration.RoomTypes.CHEST:
+					color_rect.color = Color.GOLDENROD
+				RoomGeneration.RoomTypes.BLOCK:
+					color_rect.color = Color.DIM_GRAY
+			mini_map.add_child(color_rect)
+			color_rect.position = Vector2(r, f) * 6 + mini_map_marker_2d.position
 
 func _room_entered(room):
 	var room_rect_pos = Vector2(room.position.x / ROOM_SIZE.x, room.position.y / ROOM_SIZE.y) * 6 + mini_map_marker_2d.position
